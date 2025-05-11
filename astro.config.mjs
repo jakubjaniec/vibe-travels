52f47bcd97cd3fa1a1e7f245c0f4028f01c4d1ef
jakubjaniec <jakubjaniec01@gmail.com>
2025-05-11 17:00:34 +0200

    Fix deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://vibe-travels.pages.dev",
   integrations: [react(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
